adfb81cd058ea3a2bd12a2748477b6ed922fc620
Tony Tsang <tsangh4@cardiff.ac.uk>
2025-05-31 19:18:14 +0100

    leetcode 15

---
+++ .history/2Pointers/15_20250531190726.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        seen = set()
+        answer = []
+        nums.sort()
+
+
+        for i in range(len(nums) - 3):
+
+            low = i + 1
+            high = len(nums) - 1
+            if nums[i] > 0:
+                return answer
+
+            if nums[i] in seen:
+                continue
+            else:
+                seen.add(nums[i])
+                while low < high:
+                    res = nums[i] + nums[low] + nums[high]
+                    if  res == 0:
+                        answer.append((nums[i], nums[low], nums[high]))
+
+                        while nums[high] == nums[high-1]:
+                            high -= 1
+                        while nums[low] == nums[low+1]:
+                            low += 1
+
+                    elif res < 0:
+                        low += 1
+                    elif res > 0:
+                        high -= 1
+        return answer
+
+
+
